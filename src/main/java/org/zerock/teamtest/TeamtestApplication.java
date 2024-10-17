5d2be88424486d53e754019c54bd882e1b623ed1
Sangwon Lee <sang4939@gmail.com>
2024-10-17 14:38:30 +0900

    feat: first

---
+++ src/main/java/org/zerock/teamtest/TeamtestApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.teamtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerock/teamtest/TeamtestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.teamtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
